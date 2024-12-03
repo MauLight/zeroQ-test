39783df673f0a30b682c71065d23f3fd9be92e12
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-12-03 20:35:37 -0300

    feat: first version of hook added
    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
 
 import { lazy, useLayoutEffect } from 'react'
-import axios from 'axios'
 
 //* Components
 import Topbar from './components/topbar/Topbar'
-
-const backendUrl = import.meta.env.VITE_BACKENDURL
-console.log(backendUrl)
+import useFetchSort from './hooks/useFetchSort'
 
 const Home = lazy(async () => await import('@/views/Home'))
 
 function App() {
-
-  async function getAllServices() {
-    try {
-      const { data } = await axios.get(backendUrl)
-      console.log(data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  const [offices, status] = useFetchSort()
 
   useLayoutEffect(() => {
-    getAllServices()
-  }, [])
+    console.log(offices)
+  }, [status])
 
   return (
     <div className='min-h-screen bg-zeroq-800'>
       <Topbar />
-      <Home />
+      {
+        status === 'success' && (
+          <Home />
+        )
+      }
     </div>
   )
 }
--- src/hooks/useFetchSort.tsx
+++ src/hooks/useFetchSort.tsx
@@ -1,16 +1,36 @@
 import axios from 'axios'
+import { useLayoutEffect, useState } from 'react'
 
 const backendUrl = import.meta.env.VITE_BACKENDURL
 
+interface OfficesProps {
+    id: number
+    lines: Array<{ waiting: number, elapsed: number }>
+    name: string
+    online: boolean
+}
+
 function useFetchSort() {
-    async function getAllServices() {
+    const [offices, setOffices] = useState<OfficesProps[]>([])
+    const [status, setStatus] = useState<'idle' | 'loading' | 'rejected' | 'success'>('idle')
+
+    async function getOffices() {
         try {
+            setStatus('loading')
             const { data } = await axios.get(backendUrl)
-            console.log(data)
+            setOffices(data)
+            setStatus('success')
         } catch (error) {
+            setStatus('rejected')
             console.error(error)
         }
     }
+
+    useLayoutEffect(() => {
+        getOffices()
+    }, [])
+
+    return [offices, status]
 }
 
 export default useFetchSort
